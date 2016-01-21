e2e7dd1fe4948126bb8c6d7a87f7b1f7d3662e3a
aantinori <aantinori@hospitalneuquen.org.ar>
2016-01-21 08:57:49 -0300

    valoración enfermería

--- public/app.js
+++ public/app.js
@@ -3,8 +3,16 @@
 var appModule = angular.module('appModule')
     .config(["PlexResolverProvider", function (PlexResolverProvider) {
         PlexResolverProvider
+<<<<<<< HEAD
              .when('/valoracionEnfermeria', { templateUrl: 'partials/valoracionEnfermeria.html', controller: 'ValoracionEnfermeriaController' })
              .otherwise({ redirectTo: '/valoracionEnfermeria' })
+=======
+             .when('/prueba', { templateUrl: 'partials/prueba.html', controller: 'PruebaController' })
+             .when('/camas', { templateUrl: 'partials/camas.html', controller: 'CamasController' })
+             .when('/pacientes/evolucionar', { templateUrl: 'partials/pacientes/evolucionar.html', controller: 'PacientesController' })
+             .when('/pacientes/prestaciones', { templateUrl: 'partials/pacientes/evolucionar.html', controller: 'PacientesController' })
+             .otherwise({ redirectTo: '/prueba' })
+>>>>>>> origin/master
     }])
     .run(['$rootScope', function ($rootScope) {
     }]);
