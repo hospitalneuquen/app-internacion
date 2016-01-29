4b6cf5bc348e0ba1ea0453549324d7f0a616232c
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-01-29 14:25:20 -0300

    Compatibilidad con vApp

--- public/app.js
+++ public/app.js
@@ -11,13 +11,13 @@
 angular.module('app')
     .config(["PlexResolverProvider", function (PlexResolverProvider) {
         PlexResolverProvider
-             .when('/prueba', { templateUrl: 'partials/prueba.html', controller: 'PruebaController' })
+             .when('/inicio', { templateUrl: 'partials/inicio.html', controller: 'InicioController' })
              .when('/valoracionEnfermeria', { templateUrl: 'partials/valoracionEnfermeria.html', controller: 'ValoracionEnfermeriaController' })
              .when('/mapa', { templateUrl: 'partials/mapa.html', controller: 'MapaController' })
              .when('/pacientes/evolucionar', { templateUrl: 'partials/pacientes/evolucionar.html', controller: 'PacientesController' })
              .when('/pacientes/prestaciones', { templateUrl: 'partials/pacientes/evolucionar.html', controller: 'PacientesController' })
              .when('/sandbox/personas', { templateUrl: 'partials/sandbox/personas.html', controller: 'sandbox/personas' })
-             .otherwise({ redirectTo: '/prueba' })
+             .otherwise({ redirectTo: '/inicio' })
     }])
     .run(['$rootScope', function ($rootScope) {
     }]);
