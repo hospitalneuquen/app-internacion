db35f70fa08a5f4e80dc3a082ee0c2f17cd78565
manouman <manurbano.dev@gmail.com>
2016-02-12 12:04:47 -0300

    Cambios en el maquetado del formulario de evolucion

--- public/js/controllers/pacientes/evolucionar.js
+++ public/js/controllers/pacientes/evolucionar.js
@@ -10,30 +10,46 @@ angular.module('app').controller('pacientes/evolucionar', ['$scope', 'Plex', 'pl
         init: function() {
 
             // buscamos la cama
-            Server.get("/api/internacion/cama/" + plexParams.idCama, {}, {updateUI: false}).then(function(cama){
-                if (!cama){
+            Server.get("/api/internacion/cama/" + plexParams.idCama, {}, {
+                updateUI: false
+            }).then(function(cama) {
+                if (!cama) {
                     alert("No se ha podido encontrar la cama");
-                }else{
+                } else {
                     $scope.cama = cama;
                 }
             });
 
             // buscamos la internacion
-            Server.get("/api/internacion/internacion/" + plexParams.idInternacion, {}, {updateUI: false}).then(function(internacion){
-                if (!internacion){
+            Server.get("/api/internacion/internacion/" + plexParams.idInternacion, {}, {
+                updateUI: false
+            }).then(function(internacion) {
+                if (!internacion) {
                     alert("No se ha podido encontrar la internacion");
-                }else{
-                    console.log(internacion);
+                } else {
                     $scope.internacion = internacion;
                 }
             });
 
-            // buscamos todas las evoluciones
-
             // buscamos todos los datos del paciente que faltan
         },
         guardarEvolucion: function() {
+            var dto = {
+                paciente: $scope.internacion.paciente.toString(),
+                estado: 'ingresado',
 
+                fechaHora: $scope.internacion.fechaHora,
+                tipo: $scope.internacion.tipo.id,
+                motivo: $scope.internacion.motivo,
+                diagnosticoPresuntivo: $scope.internacion.diagnosticoPresuntivo
+
+            };
+
+            Server.patch('/api/internacion/internacion', dto).then(function(data) {
+                Plex.closeView(data);
+            }, function() {
+
+            });
         }
     });
 
