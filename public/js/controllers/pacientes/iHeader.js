8b2eed83d68c1c7ce5229c593ef48e698ea5abba
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-02-25 12:54:59 -0300

    header pacientes

---
+++ public/js/controllers/pacientes/iHeader.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('app').controller('Pacientes/iHeaderController', ["$scope", function ($scope) {
+    /*
+    Este (sub)controlador espera los siguientes parametros (plex-include):
+        - internacion: object          | Objeto de internación
+
+    Responde a los siguientes eventos:
+        - Ninguno
+
+    Emite los siguientes eventos:
+        - Ninguno
+    */
+
+    angular.extend($scope, {
+        internacion: null,
+    });
+    $scope.$watch('include.internacion', function (current, old) {
+        $scope.internacion = current;
+    });
+}]);
