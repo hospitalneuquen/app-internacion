85fc91cee93532bc0d4c6159af53f6329e156491
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-03-15 15:12:19 -0300

    affix fix

--- public/js/directives/plexAffix.js
+++ public/js/directives/plexAffix.js
@@ -3,6 +3,14 @@ angular.module('app').directive('plexAffix', [
     function($window) {
         return function(scope, element) {
             // Init
+            var parent = element.parent();
+            while (parent.length && parent[0].tagName != "MD-CONTENT")
+                parent = parent.parent();
+            if (!parent.length)
+                parent = angular.element($window);
+            var toolbar = $window.document.getElementsByTagName("MD-TOOLBAR")[0];
+
+
             element.addClass('plex-affix');
             var spacer = angular.element("<DIV>").addClass('plex-affix-spacer');
             element.parent().append(spacer);
@@ -10,26 +18,28 @@ angular.module('app').directive('plexAffix', [
 
             // Check function
             var check = function() {
-                var scrollTop = $window.pageYOffset;
-                var scrollHeight = $window.document.body.scrollHeight;
+                var scrollTop = parent[0].scrollTop; //$window.pageYOffset;
                 var clientRect = element[0].getBoundingClientRect();
+                var parentRect = parent[0].getBoundingClientRect();
                 if (initialOffsetTop < 0) {
                     initialOffsetTop = clientRect.top;
                 }
 
-                if (clientRect.top <= 0 && !element.hasClass('affix')) {
+                if (clientRect.top <= parentRect.top && !element.hasClass('affix')) {
                     element.addClass('affix');
+                    element.css('top', toolbar.getBoundingClientRect().height + "px");
                     spacer.css('height', clientRect.height + "px");
                 }
-                if (scrollTop < initialOffsetTop) {
+                console.log(scrollTop);
+                if (scrollTop <= 0) {
                     element.removeClass('affix');
                 }
             };
 
             // Eventos
-            angular.element($window).on('scroll', check);
+            parent.on('scroll', check);
             scope.$on('$destroy', function() {
-                angular.element($window).off('scroll', check);
+                parent.off('scroll', check);
             });
         };
     }
