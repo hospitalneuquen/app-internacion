38264579aed966f7ff17563822246bbe6abde3cf
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-03-18 12:22:06 -0300

    ejemplo plex-chart

--- public/js/controllers/inicio.js
+++ public/js/controllers/inicio.js
@@ -1,6 +1,4 @@
-'use strict';
-
-angular.module('app').controller('InicioController', ['$scope', 'Plex', 'Server', function($scope, Plex, Server) {    
+angular.module('app').controller('InicioController', ['$scope', 'Plex', 'Server', 'Global', function($scope, Plex, Server, Global) {
     // Inicializa vista
     Plex.initView({
         title: "Punto de inicio",
