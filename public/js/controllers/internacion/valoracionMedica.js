fa35ec45ebb5885c287eb77bcf1f649619f27a58
aantinori <aantinori@hospitalneuquen.org.ar>
2016-07-05 09:45:25 -0300

    Signos, síntomas e hipótesis

--- public/js/controllers/internacion/valoracionMedica.js
+++ public/js/controllers/internacion/valoracionMedica.js
@@ -1,9 +1,8 @@
-angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Plex', 'plexParams', 'Shared', function($scope, Plex, plexParams, Shared) {
+angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Plex', 'plexParams', 'Shared', 'Session', function($scope, Plex, plexParams, Shared, Session) {
     angular.extend($scope, {
         internacion: null,
 
         init: function() {
-            // $scope.tab = plexParams.tab || 0;
             Shared.internacion.get(plexParams.idInternacion).then(function(data) {
                 $scope.internacion = data;
             });
@@ -14,8 +13,8 @@ angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Ple
             $scope.internacion.ingreso.medico.impresionDiagnostica.listaHipotesis.splice($scope.internacion.ingreso.medico.impresionDiagnostica.listaHipotesis.indexOf(hipotesis), 1);
         },
 
-        removeSindrome: function(sindrome) {
-            $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromes.splice($scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromes.indexOf(sindrome), 1);
+        removeSindromeSignos: function(sindrome) {
+            $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.splice($scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.indexOf(sindrome), 1);
         },
 
         removeSignoGuia: function(positivo) {
@@ -38,12 +37,57 @@ angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Ple
             $scope.hipotesis = {};
         },
 
+        confirmarHipotesis: function(hipotesis){
+            if (typeof $scope.hipotesis == "undefined") {
+                $scope.hipotesis = {};
+            }
+            $scope.hipotesis.confirmada = true;
+            for (var i = 0; i < $scope.internacion.ingreso.medico.impresionDiagnostica.listaHipotesis.length; i++) {
+                if ($scope.internacion.ingreso.medico.impresionDiagnostica.listaHipotesis[i].nombre === hipotesis.nombre) {
+                    // hipotesis encontrada, actualizamos datos
+                    $scope.internacion.ingreso.medico.impresionDiagnostica.listaHipotesis[i].confirmada = $scope.hipotesis.confirmada;
+                    break;
+                }
+            }
+            if (typeof $scope.problema == "undefined") {
+                $scope.problema = {};
+            }
+
+            $scope.problema.diagnosticoTexto = hipotesis.nombre;
+            $scope.problema.descripcion = hipotesis.nombre;
+            $scope.problema.estado = "Activo";
+            $scope.problema.servicio = Session.variables.servicioActual;
+            $scope.problema.fechaDesde = new Date();
+            $scope.internacion.problemas.push($scope.problema);
+
+        },
+
+        agregarSindromeSignos: function() {
+            if (typeof $scope.internacion.ingreso.medico == "undefined") {
+                $scope.internacion.ingreso.medico = {};
+            }
+            if (typeof $scope.internacion.ingreso.medico.impresionDiagnostica == "undefined") {
+                $scope.internacion.ingreso.medico.impresionDiagnostica = {};
+            }
+            if (typeof $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos == "undefined") {
+                $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos = [];
+            }
+            if (typeof $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.listaSignos == "undefined") {
+                $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.listaSignos = [];
+            }
+
+            angular.forEach($scope.sindrome.listaSignos, function(signo) {
+                $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.listaSignos.push(signo);
+            });
+            $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromesSignos.push($scope.sindrome);
+
+            $scope.sindrome = {};
+        },
+
         checkIfEnterKeyWasPressed: function($event, input) {
-            // console.log($event.which);
             var keyCode = $event.which || $event.keyCode;
             if (keyCode === 13) {
                 if (input == 1 && $scope.positivo !== "") {
-                    // Do that thing you finally wanted to do
                     if (typeof $scope.internacion.ingreso.medico == "undefined") {
                         $scope.internacion.ingreso.medico = {};
                     }
@@ -58,22 +102,6 @@ angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Ple
 
                     $scope.positivo = "";
                 }
-                if (input == 2 && $scope.sindrome !== "") {
-                    // Do that thing you finally wanted to do
-                    if (typeof $scope.internacion.ingreso.medico == "undefined") {
-                        $scope.internacion.ingreso.medico = {};
-                    }
-                    if (typeof $scope.internacion.ingreso.medico.impresionDiagnostica == "undefined") {
-                        $scope.internacion.ingreso.medico.impresionDiagnostica = {};
-                    }
-                    if (typeof $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromes == "undefined") {
-                        $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromes = [];
-                    }
-
-                    $scope.internacion.ingreso.medico.impresionDiagnostica.listaSindromes.push($scope.sindrome);
-
-                    $scope.sindrome = "";
-                }
             }
 
         },
@@ -82,13 +110,36 @@ angular.module('app').controller('internacion/valoracionMedica', ['$scope', 'Ple
             var data = {
                 ingreso: $scope.internacion.ingreso
             };
+            if ($scope.problema){
+                Shared.problemas.post($scope.internacion.id, $scope.problema.id || null, $scope.problema, {
+                    minify: true
+                }).then(function(){
+                    Plex.alert("Valoración médica guardada. Recuerde editar la lista de problemas si desea agregar la codificación CIE-10 de la hipótesis confirmada (problema activo).");
+                    Plex.closeView();
+                });
+            }
+            else{
+                return Shared.internacion.post(plexParams.idInternacion, data).then(function(internacion) {
+                    if (internacion) {
+                        Plex.alert("Valoración médica guardada");
+                    }
+                    Plex.closeView(internacion);
+                });
+            }
 
-            return Shared.internacion.post(plexParams.idInternacion, data).then(function(internacion) {
-                if (internacion) {
-                    Plex.alert("Valoración médica guardada");
-                }
-                Plex.closeView(internacion);
-            });
+            // Shared.internacion.post(plexParams.idInternacion, data).then(function(internacion) {
+            //     if (internacion) {
+            //         Plex.alert("Valoración médica guardada");
+            //     }
+            //     Plex.closeView(internacion);
+            // });
+
+            // return Shared.internacion.post(plexParams.idInternacion, data).then(function(internacion) {
+            //     if (internacion) {
+            //         Plex.alert("Valoración médica guardada");
+            //     }
+            //     Plex.closeView(internacion);
+            // });
         },
         cancelar: function() {
             Plex.closeView();
