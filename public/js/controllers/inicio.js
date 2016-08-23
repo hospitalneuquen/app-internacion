5fb5daaec65896bb216b25178d746d225831ce14
manouman <manurbano.dev@gmail.com>
2016-08-23 09:47:05 -0300

    Agregados indicadores del estado del servicio en el punto de inicio.

--- public/js/controllers/inicio.js
+++ public/js/controllers/inicio.js
@@ -1,6 +1,48 @@
 'use strict';
 
-angular.module('app').controller('InicioController', ['$scope', 'Plex', 'Server', function($scope, Plex, Server) {
+angular.module('app').controller('InicioController', ['$scope', 'Plex', 'Shared', 'Session', function($scope, Plex, Shared,Session) {
+
+    angular.extend($scope, {
+        init: function() {
+            // obtenemos las camas para armar el mapa
+            Shared.Mapa.get(Session.variables.servicioActual.id).then(function(data) {
+                $scope.camas = data;
+
+                $scope.camasOcupadas = $scope.camas.filter(function(i){
+                    return (i.estado == 'ocupada')
+                });
+
+                // ocupacion
+                $scope.camasBloqueadas = $scope.camas.filter(function(i){
+                    return (i.estado == 'bloqueada')
+                });
+
+                // deshabilitadas
+                $scope.camasBloqueadas = $scope.camas.filter(function(i){
+                    return (i.estado == 'bloqueada')
+                });
+
+                $scope.camasDescontaminacion = $scope.camas.filter(function(i){
+                    return (i.estado == 'desocupada' && !i.desinfectada)
+                });
+
+                $scope.camasReparacion = $scope.camas.filter(function(i){
+                    return (i.estado == 'reparacion')
+                });
+
+                // disponibles
+                $scope.camasDesocupadas = $scope.camas.filter(function(i){
+                    return (i.estado == 'desocupada')
+                });
+
+                $scope.camasDesocupadasOxigeno = $scope.camas.filter(function(i){
+                    return (i.estado == 'desocupada' && i.oxigeno)
+                });
+            });
+
+        }
+    });
+
     // Inicializa vista
     Plex.initView({
         title: "Punto de inicio",
@@ -12,4 +54,6 @@ angular.module('app').controller('InicioController', ['$scope', 'Plex', 'Server'
             }
         }]
     });
+
+    $scope.init();
 }]);
